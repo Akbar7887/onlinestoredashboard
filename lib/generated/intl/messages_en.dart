3d858d26866d7d3b8c62e404ede0623a52f2f88c
Akbar <akbar358888@gmail.com>
2023-02-27 10:43:09 +0500

    i

---
+++ lib/generated/intl/messages_en.dart
@@ -0,0 +1,37 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a en locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'en';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "add": MessageLookupByLibrary.simpleMessage("Добавить"),
+        "cancel": MessageLookupByLibrary.simpleMessage("Отмена"),
+        "catalog": MessageLookupByLibrary.simpleMessage("Каталог"),
+        "catalog_show_diagram": MessageLookupByLibrary.simpleMessage(
+            "Форма для измениие или добавление!"),
+        "delete": MessageLookupByLibrary.simpleMessage("Удалить"),
+        "edit": MessageLookupByLibrary.simpleMessage("Изменить"),
+        "name": MessageLookupByLibrary.simpleMessage("Наименование"),
+        "parent": MessageLookupByLibrary.simpleMessage("Родитель"),
+        "save": MessageLookupByLibrary.simpleMessage("Сохранить"),
+        "validate": MessageLookupByLibrary.simpleMessage("Просим заплнить поля")
+      };
+}
