870fc1078304e1de2b41e44e48f37f26a75d12b0
Akbar <akbar358888@gmail.com>
2023-03-14 15:32:14 +0500

    i

--- lib/models/catalogs/ProductImage.dart
+++ lib/models/catalogs/ProductImage.dart
@@ -1,17 +1,23 @@
+import 'Product.dart';
+
 class ProductImage {
   int? id;
   String? imagepath;
   bool? mainimg;
+  Product? product;
 
   ProductImage({
       this.id, 
       this.imagepath, 
-      this.mainimg,});
+      this.mainimg,
+  this.product});
 
   ProductImage.fromJson(dynamic json) {
     id = json['id'];
     imagepath = json['imagepath'];
     mainimg = json['mainimg'];
+    product =
+    json['product'] != null ? Product.fromJson(json['product']) : null;
   }
 
 
@@ -20,6 +26,9 @@ class ProductImage {
     map['id'] = id;
     map['imagepath'] = imagepath;
     map['mainimg'] = mainimg;
+    if (this.product != null) {
+      map['product'] = this.product!.toJson();
+    }
     return map;
   }
 
